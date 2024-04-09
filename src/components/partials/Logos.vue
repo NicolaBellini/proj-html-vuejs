c54cd241483a2137721283076b00f5af0de752de
NicolaBellini <bellininicola4366@gmail.com>
2024-04-09 11:20:45 +0200

    [add] logos images

--- src/components/partials/Logos.vue
+++ src/components/partials/Logos.vue
@@ -5,12 +5,24 @@ export default {};
 <template>
   <div class="wrapper">
     <div class="my-container">
-      <div class="logo debug"></div>
-      <div class="logo debug"></div>
-      <div class="logo debug"></div>
-      <div class="logo debug"></div>
-      <div class="logo debug"></div>
-      <div class="logo debug"></div>
+      <div class="logo">
+        <img src="../../../public/img/logo-1.png" alt="" />
+      </div>
+      <div class="logo">
+        <img src="../../../public/img/logo-2.png" alt="" />
+      </div>
+      <div class="logo">
+        <img src="../../../public/img/logo-3.png" alt="" />
+      </div>
+      <div class="logo">
+        <img src="../../../public/img/logo-4.png" alt="" />
+      </div>
+      <div class="logo">
+        <img src="../../../public/img/logo-5.png" alt="" />
+      </div>
+      <div class="logo">
+        <img src="../../../public/img/logo-1.png" alt="" />
+      </div>
     </div>
   </div>
 </template>
@@ -24,10 +36,19 @@ export default {};
   .my-container {
     display: flex;
     align-items: center;
+    // filter: grayscale(100%);
+    filter: invert(80%);
     .logo {
+      display: flex;
+      align-items: center;
+      justify-content: center;
       height: 40px;
       margin: 0 40px;
       width: calc(100% / 6);
+      img {
+        max-height: 100%;
+        max-width: 100%;
+      }
     }
   }
 }
