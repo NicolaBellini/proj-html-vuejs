aa299c072c7427c08087e6dc8e5d613748ba4d7f
NicolaBellini <Bellininicola4366@gmail.com>
2024-07-15 09:46:13 +0200

    [fix] header responsive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -18,19 +18,26 @@ export default {
     <div class="container d-flex">
       <div class="orari d-flex">
         <span>
-          <i class="fa-solid fa-clock"></i> Open Hours
-          {{ store.contacts.orari }}
+          <i class="fa-solid fa-clock"></i>
+          <span>
+            Open Hours
+            {{ store.contacts.orari }}
+          </span>
         </span>
       </div>
       <!-- per mettere i loghi scrivere vhtml -->
       <!-- <p v-html="social.twitter"></p> -->
       <div class="contact d-flex">
         <span
-          ><i class="fa-solid fa-phone"></i> {{ store.contacts.phone }}</span
-        >
+          ><i class="fa-solid fa-phone"></i>
+          <span> {{ store.contacts.phone }}</span>
+        </span>
         <span
-          ><i class="fa-solid fa-envelope"></i> {{ store.contacts.mail }}</span
-        >
+          ><i class="fa-solid fa-envelope"></i>
+          <span>
+            {{ store.contacts.mail }}
+          </span>
+        </span>
         <a v-html="store.contacts.social.facebook"></a>
         <a v-html="store.contacts.social.twitter"></a>
         <a v-html="store.contacts.social.linkedin"></a>
@@ -44,6 +51,7 @@ export default {
 <style lang="scss" scoped>
 @use "../assets/scss/partials/variables" as *;
 @import "../assets/scss/main.scss";
+
 .wrapper {
   background-color: $contacts_color;
   color: $paragraph;
@@ -51,21 +59,87 @@ export default {
   .container {
     height: 50px;
     font-size: 0.7rem;
+    display: flex;
     justify-content: space-between;
     align-items: center;
+
     .orari {
       width: 50%;
+      display: flex;
       align-items: center;
+
       span {
         color: $paragraph;
       }
     }
+
     .contact {
       width: 40%;
+      display: flex;
       justify-content: space-between;
       align-items: center;
+
       span {
         color: $paragraph;
+      }
+
+      a {
+        color: $paragraph;
+        text-decoration: none;
+        margin-left: 10px;
+      }
+    }
+  }
+
+  @media (max-width: 1024px) {
+    .container {
+      flex-direction: column;
+      align-items: flex-start;
+      height: auto;
+      padding: 10px 0;
+
+      .orari,
+      .contact {
+        width: 100%;
+        margin-bottom: 10px;
+      }
+
+      .contact {
+        justify-content: flex-start;
+
+        span {
+          margin-right: 15px;
+          span {
+            display: none;
+          }
+        }
+
+        a {
+          margin-right: 15px;
+        }
+      }
+    }
+  }
+
+  @media (max-width: 768px) {
+    .container {
+      padding: 5px 0;
+
+      .orari,
+      .contact {
+        align-items: center;
+        justify-content: center;
+      }
+
+      .contact {
+        flex-wrap: wrap;
+
+        span,
+        a {
+          i {
+            margin: 5px 0;
+          }
+        }
       }
     }
   }
